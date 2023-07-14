121829f8baf4eaaa7e1f4784076cf01fca9950cb
KonstantinSeilerVinteo <k.seiler@vinteo.ru>
2023-07-14 10:19:55 +0300

    someShlyapa

--- workFlow.js
+++ workFlow.js
@@ -1,6 +1,10 @@
 
 let numberTestCase;
 let countRewiev;
+let countTestCase;
+let answerWhoAreYou;
+let realizeFeature;
+let answerNotFeature;
 
 UpdateRewiev();
 tryCountRewiev();
@@ -13,7 +17,7 @@ function UpdateRewiev() {
     
 function tryCountRewiev() {
      const userAnswer = parseInt(prompt('Введите количество ревью, которое уже прошли. Для  выхода из этого прекрасного меню наберите -1.'));
-      
+     
      if (userAnswer === -1) {
         alert ('Сказочник');
 
@@ -34,68 +38,48 @@ function tryCountRewiev() {
 
 
     if (userAnswer === numberTestCaseMax) {
-        const countTestCase = confirm('Значит ты у нас тестировщик, и написал тестовые сценарии?');
+        countTestCase = confirm('Значит ты у нас тестировщик, и написал тестовые сценарии?');
 
-        if (countTestCase === true) {
-            return alert ('Отлично, отправляй Тимлиду');
-        }
-
-        if (countTestCase === false) {
-            prompt ('Кто ты воин? Разработчик, Тимлид, Рюма');
-
-            return tryCountRewiev();
-        }
     }
-}
 
+
+    if (countTestCase === true) {
+        alert ('Отлично, отправляй Тимлиду');
+
+        return tryCountRewiev();
+
+    }
+    
+    
+    if (countTestCase === false) {
+        answerWhoAreYou = prompt ('Кто ты воин? Разработчик, Тимлид, Рюма');
+    
+    }
+    
+    
+     if (answerWhoAreYou === 'Разработчик') {
+        realizeFeature = confirm('Реализовал фичу?')
+     }
+
+
+    if (realizeFeature === true) {
+        return alert ('Твоя карточка заапрувлена!');
+    }
+            
+            
+    if (realizeFeature === false) {
+        answerNotFeature = confirm('Ты автоматизировал?');
+    }
+
+    if (answerNotFeature === true) {
+        alert ('Твоя карточка заапрувлена!');
+    }
+            
+                    return tryCountRewiev();
+                }
+
+            
+        
+  
 
 UpdateRewiev();
-
-
-
-// attempts++;
-
-// if (userAnswer === numberTestCase || userAnswer != 0) {
-//     const countTestCase = confirm('Значит ты у нас тестировщик, и написал тестовые сценарии?');
-//     if (countTestCase === true) {
-//         return alert ('Отлично, отправляй Тимлиду');
-
-//     } else if (userAnswer === 0) {
-//         return alert ('Отправь на ревью тестировщику!');
-//     }
-
-// } else if  (userAnswer <= 2) {
-//     return alert ('Твоя карточка заапрувлена!');
-// }
-    
-// // UpdateRewiev();
-
-
-// tryCountRewiev();
-
-
-
-
-//     confirm ('Значит ты у нас тестировщик?');
-//     return tryGuessNumber();
-
-
-
-
-//     attempts++;
-
-// } else if (userAnswer < number) {
-//     alert('Попробуй число больше.');
-//     return tryGuessNumber();
-
-// } else {
-//     alert(`Поздравляю! Вы угадали число с ${attempts} попытки.`);
-//     confirm('Играешь, бабки остались?');
-//     return alert ('Бай, бай неудачник!')
-// }
-
-// resetGame(); 
-
-// }
-
-// tryGuessNumber();
