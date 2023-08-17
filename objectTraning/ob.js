c6b670821ab26e9b89988ed7244f26742a393d30
KonstantinSeilerVinteo <k.seiler@vinteo.ru>
2023-08-17 11:51:28 +0300

    chore object

---
+++ objectTraning/ob.js
@@ -0,0 +1,16 @@
+
+function toBeep () {
+    prompt ('Машина подает сигнал?');
+    if ( prompt = typeof(Number))
+    return alert ('Цифры') 
+}
+
+
+const myCar = {
+    manufacturer: 'BNW',
+    color: 'black',
+    beep : toBeep,
+
+};
+
+myCar.beep();
