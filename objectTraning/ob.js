87bd8bdbfbc181aeed25f3d866b94a19a04c8b4c
KonstantinSeilerVinteo <k.seiler@vinteo.ru>
2023-08-21 11:50:23 +0300

    chore ojbect

--- objectTraning/ob.js
+++ objectTraning/ob.js
@@ -1,16 +1,38 @@
-
-function toBeep () {
-    prompt ('Машина подает сигнал?');
-    if ( prompt = typeof(Number))
-    return alert ('Цифры') 
-}
+// Функции записанные внутри объекта - НАЗВАЕТСЯ МЕТОД 
 
 
 const myCar = {
     manufacturer: 'BNW',
     color: 'black',
-    beep : toBeep,
-
+   possibleColors: ['black', 'red', 'brown', 'grey'],
+   engine: {
+    engineCode: 'B58',
+    power: 350,
+   }, 
+   beep() {
+    console.log('Машина подает синал!');
+   },
 };
 
-myCar.beep();
+// Вывод свойств с их значениями
+for (const prop in myCar){
+    console.log(`В объекте есть свойство ${prop}, в свойстве лежит значение: ${myCar[prop]}`)
+}
+
+
+// // Дай мне все ключи объекта myCar
+console.log(Object.keys(myCar));
+
+
+
+let a = {
+    number: 1,
+};
+console.log(a); // 1
+incOne(a);
+console.log(a); // 1
+
+
+function incOne(param) {
+    return param.number++;
+}
