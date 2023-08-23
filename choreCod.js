99ab04821a0cecf8ff367564c87fea461d69fa30
KonstantinSeilerVinteo <k.seiler@vinteo.ru>
2023-08-23 12:04:29 +0300

    ss

---
+++ choreCod.js
@@ -0,0 +1,11 @@
+// let tempTc = prompt("Введите температуру по Цельсию")
+
+// alert(9/5 * tempTc + 32);
+
+// let admin = 'Василий';
+// let name = admin;
+
+// console.log(name)
+// let numberBillet = parseInt(prompt('Введите номер билет'));
+
+// let arr = [];
