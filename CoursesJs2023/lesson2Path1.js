420cf7ca77e7a76a9ecf8b9b57a093fefeb96a8f
KonstantinSeilerVinteo <k.seiler@vinteo.ru>
2023-08-24 11:59:50 +0300

    chore calculate

--- CoursesJs2023/lesson2Path1.js
+++ CoursesJs2023/lesson2Path1.js
@@ -1,2 +1,40 @@
 const resultSumTestId = document.getElementById('result');
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const submitBtn = document.getElementById('submit');
+const plusBtn = document.getElementById('plus');
+const minusBtn = document.getElementById('minus');
+let action = '+';
+
+
+plusBtn.onclick = function () {
+    action = '+'
+}
+minusBtn.onclick = function () {
+    action = '-'
+}
+
+function printResult(result) {
+    if (result === '+') {
+        resultSumTestId.style.color = 'red';
+    } else {
+        resultSumTestId.style.color = 'green';
+    }
+    resultSumTestId.textContent = result;
+}
+
+submitBtn.onclick = function () {
+    if (action === '+') {
+    const sum = Number(input1.value) + Number(input2.value);
+    printResult(result);
+    resultSumTestId.textContent = sum;
+    } else {
+        const sum = Number(input1.value) - Number(input2.value);
+        printResult(result);
+        resultSumTestId.textContent = sum;
+        }
+}
+
+
+
 console.log(resultSumTestId);
